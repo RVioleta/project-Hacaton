91d6fc13442677b12462ec29a073463c10d225c6
violeta <ristova.violeta82@gmail.com>
2023-09-07 22:20:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//  document.getElementById("openFormButton").addEventListener("click", function() {
+   
+//     let speakPageUrl = "becomeSpeaker.html";   
+    
+//     window.open(speakPageUrl, "_blank");
+//   });
+
+
+//   ///////////////////////////////////////////////
+
+//  document.getElementById("sponsor").addEventListener("click", function() {
+   
+//     let sponsorPageUrl = "becomeSponsor.html";   
+    
+//     window.open(sponsorPageUrl, "_blank");
+//   });
+//   console.log(sponsorPageUrl)
+
+/////////////////////////////////////////
+
+  document.getElementById("welcomeSponsor").addEventListener("click", function() {
+   
+    let welcomePageUrl = "welcome.html";   
+    
+    window.open(welcomePageUrl, "_blank");
+  });
+ 
